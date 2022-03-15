ef02fb5ae270a07d1cfc7ab783ca7f59a507df2f
delaregita <101648592+delaregita@users.noreply.github.com>
2022-03-15 22:40:52 +0700

    First push

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+	public function index()
+	{
+		echo '<div align="center">';
+		echo '<pre>Nama saya Dela Regita Cahyani</pre>';
+		echo '</div>';
+	}
+}
